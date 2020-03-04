85308638b803f35be2dbe0874c2249bae07c0a45
Ahmed Hussein <wolktheline@yahoo.com>
2020-03-04 03:53:31 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,9 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   
   def change 
-    add_column :students do |x|
-      x.string :birthdate 
-      x.integer :grade 
+    add_column :grade
     end 
   end 
 end 
